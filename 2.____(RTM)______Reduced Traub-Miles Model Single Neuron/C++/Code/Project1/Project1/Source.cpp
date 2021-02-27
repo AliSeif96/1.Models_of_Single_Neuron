59a9e5db48a0b4b806e298b9d45ada89cd19f6d6
Ali Seif <65570362+aliseif321@users.noreply.github.com>
2021-02-27 21:39:53 +0330

    Update Source.cpp

--- 2.____(RTM)______Reduced Traub-Miles Model Single Neuron/C++/Code/Project1/Project1/Source.cpp
+++ 2.____(RTM)______Reduced Traub-Miles Model Single Neuron/C++/Code/Project1/Project1/Source.cpp
@@ -1,10 +1,10 @@
 /************************************************************************************************/
-/*** Topic: Wang-Buzsaki model with Runge-Kutta 4th Order Method for one neuron    Ali-Seif   ***/
+/*** Topic: Reduced Traub-Miles model with Runge-Kutta 4th Order Method for one neuron    Ali-Seif   ***/
 /*** Version Release 17.12 rev 11256                                                          ***/
 /*** Date: 11/10/2020                                                                         ***/
 /*** Code implemented in CodeBlocks C++ compiler (v. 17.12),                                  ***/
 /*** MSI: PX60 6QD/ DDR4                                                                      ***/
-/*** Run under a Intel® Core™ i7-6700HQ CPU @ 2.60GHz × 64 based processor with 16 GB RAM     ***/
+/*** Run under a IntelÂ® Coreâ„¢ i7-6700HQ CPU @ 2.60GHz Ã— 64 based processor with 16 GB RAM     ***/
 /************************************************************************************************/
 #include <iostream>
 #include <math.h>
